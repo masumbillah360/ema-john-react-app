caae9c520ec3d32e296538c05c312f106cd30df3
masumbillah360 <masumbillah6584@gmail.com>
2022-09-25 20:16:06 +0600

    upload structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./compo/Header/Header";
+import Shop from "./compo/Shop/Shop";
+
+function App() {
+  return <div>
+    <Header></Header>
+    <h1>I'm Still Existing...</h1>
+    <Shop></Shop>
+  </div>;
+}
+
+export default App;
---
+++ src/compo/Shop/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Product from '../Porducts/Product';
+
+const Shop = () => {
+    return (
+        <div>
+            <h1>From Shop..</h1>
+            <Product></Product>
+        </div>
+    );
+};
+
+export default Shop;
